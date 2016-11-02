8a70ae6a838be3c2e74086c03e0b43d67fa68b0f
William Swanson <swansontec@gmail.com>
2016-11-02 16:33:56 -0700

    cli: Add the wallet-list command

---
+++ cli/commands/wallet.js
@@ -0,0 +1,14 @@
+const command = require('../command.js')
+
+command('wallet-list', {
+  help: 'Lists the wallets in an account',
+  needsAccount: true
+}, function (session, argv) {
+  if (argv.length !== 0) throw this.usageError()
+
+  const ids = session.account.listWalletIds()
+  for (var i of ids) {
+    var wallet = session.account.getWallet(i)
+    console.log(i + ' (' + wallet.type + ') = ' + JSON.stringify(wallet.repoKeys, null, 2))
+  }
+})
