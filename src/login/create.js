EdgeApp/edge-core-js

commit fb374bd23f57c2c26922183c11b1f16523fb20c3

Make login setup code reusable
We generate keys and boxes in a separate function from the actual
uploading and saving, since account creation can also use this logic.

src/login/create.js
@@ -2,6 +2,7 @@ import * as crypto from '../crypto.js'
 import * as userMap from '../userMap.js'
 import {UserStorage} from '../userStorage.js'
 import {Login} from './login.js'
+import * as passwordLogin from './password.js'
 
 /**
  * Determines whether or not a username is available.
@@ -23,32 +24,26 @@ export function create (ctx, username, password, opts) {
   username = userMap.normalize(username)
   const userId = userMap.getUserId(ctx.localStorage, username)
 
-  // Create random key material:
-  const passwordKeySnrp = crypto.makeSnrp()
+  // Create account repo info:
   const dataKey = crypto.random(32)
   const syncKey = opts.syncKey || crypto.random(20)
-
-  // Derive keys from password:
-  const passwordAuth = crypto.scrypt(username + password, crypto.passwordAuthSnrp)
-  const passwordKey = crypto.scrypt(username + password, passwordKeySnrp)
-
-  // Encrypt:
-  const passwordBox = crypto.encrypt(dataKey, passwordKey)
-  const passwordAuthBox = crypto.encrypt(passwordAuth, dataKey)
   const syncKeyBox = crypto.encrypt(syncKey, dataKey)
+
+  // Create password login info:
+  const passwordSetup = passwordLogin.makeSetup(ctx, dataKey, username, password)
 
   // Package:
   const carePackage = {
-    'SNRP2': passwordKeySnrp
+    'SNRP2': passwordSetup.server.passwordKeySnrp
   }
   const loginPackage = {
-    'EMK_LP2': passwordBox,
+    'EMK_LP2': passwordSetup.server.passwordBox,
     'ESyncKey': syncKeyBox,
-    'ELP1': passwordAuthBox
+    'ELP1': passwordSetup.server.passwordAuthBox
   }
   const request = {
     'l1': userId,
-    'lp1': passwordAuth.toString('base64'),
+    'lp1': passwordSetup.server.passwordAuth,
     'care_package': JSON.stringify(carePackage),
     'login_package': JSON.stringify(loginPackage),
     'repo_account_key': syncKey.toString('hex')
@@ -58,18 +53,13 @@ export function create (ctx, username, password, opts) {
     // Cache everything for future logins:
     userMap.insert(ctx.localStorage, username, userId)
     const userStorage = new UserStorage(ctx.localStorage, username)
-    userStorage.setJson('passwordKeySnrp', passwordKeySnrp)
-    userStorage.setJson('passwordBox', passwordBox)
-    userStorage.setJson('passwordAuthBox', passwordAuthBox)
+    userStorage.setItems(passwordSetup.storage)
     userStorage.setJson('syncKeyBox', syncKeyBox)
 
+    const login = Login.offline(ctx.localStorage, username, dataKey)
+
     // Now activate:
-    const request = {
-      'l1': userId,
-      'lp1': passwordAuth.toString('base64')
-    }
-    return ctx.authRequest('POST', '/v1/account/activate', request).then(reply => {
-      return Login.offline(ctx.localStorage, username, dataKey)
-    })
+    const request = login.authJson()
+    return ctx.authRequest('POST', '/v1/account/activate', request).then(reply => login)
   })
 }

src/login/password.js
@@ -86,33 +86,48 @@ export function check (ctx, login, password) {
 }
 
 /**
+ * Creates the data needed to set up the password on the server.
+ */
+export function makeSetup (ctx, dataKey, username, password) {
+  const up = username + password
+
+  // dataKey chain:
+  const passwordKeySnrp = crypto.makeSnrp()
+  const passwordKey = crypto.scrypt(up, passwordKeySnrp)
+  const passwordBox = crypto.encrypt(dataKey, passwordKey)
+
+  // authKey chain:
+  const passwordAuth = crypto.scrypt(up, crypto.passwordAuthSnrp)
+  const passwordAuthBox = crypto.encrypt(passwordAuth, dataKey)
+
+  return {
+    server: {
+      'passwordAuth': passwordAuth.toString('base64'),
+      'passwordAuthSnrp': crypto.passwordAuthSnrp, // TODO: Not needed
+      'passwordKeySnrp': passwordKeySnrp,
+      'passwordBox': passwordBox,
+      'passwordAuthBox': passwordAuthBox
+    },
+    storage: {
+      'passwordKeySnrp': passwordKeySnrp,
+      'passwordBox': passwordBox,
+      'passwordAuthBox': passwordAuthBox
+    },
+    passwordAuth
+  }
+}
+
+/**
  * Sets up a password for the login.
  */
 export function setup (ctx, login, password) {
-  const up = login.username + password
-
-  // Create new keys:
-  const passwordAuth = crypto.scrypt(up, crypto.passwordAuthSnrp)
-  const passwordKeySnrp = crypto.makeSnrp()
-  const passwordKey = crypto.scrypt(up, passwordKeySnrp)
-
-  // Encrypt:
-  const passwordBox = crypto.encrypt(login.dataKey, passwordKey)
-  const passwordAuthBox = crypto.encrypt(passwordAuth, login.dataKey)
+  const setup = makeSetup(ctx, login.dataKey, login.username, password)
 
   const request = login.authJson()
-  request['data'] = {
-    'passwordAuth': passwordAuth.toString('base64'),
-    'passwordAuthSnrp': crypto.passwordAuthSnrp, // TODO: Not needed
-    'passwordKeySnrp': passwordKeySnrp,
-    'passwordBox': passwordBox,
-    'passwordAuthBox': passwordAuthBox
-  }
+  request['data'] = setup.server
   return ctx.authRequest('PUT', '/v2/login/password', request).then(reply => {
-    login.userStorage.setJson('passwordKeySnrp', passwordKeySnrp)
-    login.userStorage.setJson('passwordBox', passwordBox)
-    login.userStorage.setJson('passwordAuthBox', passwordAuthBox)
-    login.passwordAuth = passwordAuth
+    login.userStorage.setItems(setup.storage)
+    login.passwordAuth = setup.passwordAuth
     return null
   })
 }

src/login/pin2.js
@@ -58,9 +58,9 @@ export function login (ctx, pin2Key, username, pin) {
 }
 
 /**
- * Sets up PIN login v2.
+ * Creates the data needed to set up a PIN on the account.
  */
-export function setup (ctx, login, pin) {
+export function makeSetup (ctx, login, pin) {
   let pin2Key = login.userStorage.getItem('pin2Key')
   if (pin2Key) {
     pin2Key = base58.decode(pin2Key)
@@ -71,16 +71,30 @@ export function setup (ctx, login, pin) {
   const pin2Box = crypto.encrypt(login.dataKey, pin2Key)
   const pin2KeyBox = crypto.encrypt(pin2Key, login.dataKey)
 
-  const request = login.authJson()
-  request['data'] = {
-    'pin2Id': pin2Id(pin2Key, login.username).toString('base64'),
-    'pin2Auth': pin2Auth(pin2Key, pin).toString('base64'),
-    'pin2Box': pin2Box,
-    'pin2KeyBox': pin2KeyBox
+  return {
+    server: {
+      'pin2Id': pin2Id(pin2Key, login.username).toString('base64'),
+      'pin2Auth': pin2Auth(pin2Key, pin).toString('base64'),
+      'pin2Box': pin2Box,
+      'pin2KeyBox': pin2KeyBox
+    },
+    storage: {
+      'pin2Key': base58.encode(pin2Key)
+    },
+    pin2Key
   }
+}
+
+/**
+ * Sets up PIN login v2.
+ */
+export function setup (ctx, login, pin) {
+  const setup = makeSetup(ctx, login, pin)
+
+  const request = login.authJson()
+  request['data'] = setup.server
   return ctx.authRequest('PUT', '/v2/login/pin2', request).then(reply => {
-    pin2Key = base58.encode(pin2Key)
-    login.userStorage.setItem('pin2Key', pin2Key)
-    return pin2Key
+    login.userStorage.setItems(setup.storage)
+    return base58.encode(setup.pin2Key)
   })
 }

src/login/recovery2.js
@@ -83,9 +83,9 @@ export function questions (ctx, recovery2Key, username) {
 }
 
 /**
- * Sets up recovery questions for the login.
+ * Creates the data needed to set up recovery questions on the account.
  */
-export function setup (ctx, login, questions, answers) {
+export function makeSetup (ctx, login, questions, answers) {
   if (!(Object.prototype.toString.call(questions) === '[object Array]')) {
     throw new TypeError('Questions must be an array of strings')
   }
@@ -104,18 +104,32 @@ export function setup (ctx, login, questions, answers) {
   const recovery2Box = crypto.encrypt(login.dataKey, recovery2Key)
   const recovery2KeyBox = crypto.encrypt(recovery2Key, login.dataKey)
 
-  const request = login.authJson()
-  request['data'] = {
-    'recovery2Id': recovery2Id(recovery2Key, login.username).toString('base64'),
-    'recovery2Auth': recovery2Auth(recovery2Key, answers),
-    'recovery2Box': recovery2Box,
-    'recovery2KeyBox': recovery2KeyBox,
-    'question2Box': question2Box
+  return {
+    server: {
+      'recovery2Id': recovery2Id(recovery2Key, login.username).toString('base64'),
+      'recovery2Auth': recovery2Auth(recovery2Key, answers),
+      'recovery2Box': recovery2Box,
+      'recovery2KeyBox': recovery2KeyBox,
+      'question2Box': question2Box
+    },
+    storage: {
+      'recovery2Key': base58.encode(recovery2Key)
+    },
+    recovery2Key
   }
+}
+
+/**
+ * Sets up recovery questions for the login.
+ */
+export function setup (ctx, login, questions, answers) {
+  const setup = makeSetup(ctx, login, questions, answers)
+
+  const request = login.authJson()
+  request['data'] = setup.server
   return ctx.authRequest('PUT', '/v2/login/recovery2', request).then(reply => {
-    recovery2Key = base58.encode(recovery2Key)
-    login.userStorage.setItem('recovery2Key', recovery2Key)
-    return recovery2Key
+    login.userStorage.setItems(setup.storage)
+    return base58.encode(setup.recovery2Key)
   })
 }
 

src/util/scopedStorage.js
@@ -49,3 +49,16 @@ ScopedStorage.prototype.removeAll = function () {
     this.removeItem(key)
   }
 }
+
+ScopedStorage.prototype.setItems = function (items) {
+  for (const key in items) {
+    if (items.hasOwnProperty(key)) {
+      const item = items[key]
+      if (typeof item === 'string') {
+        this.setItem(key, item)
+      } else {
+        this.setJson(key, item)
+      }
+    }
+  }
+}

commit dab1534a862ffd955e4e1fcbed83a59e11a33e6e

Fix a bug in account creation
This bug was introduced in commit fb374bd23f57c2c26922183c11b1f16523fb20c3.
The activation endpoint is in the v1 API, so it needs special handling.

src/login/create.js
@@ -62,7 +62,11 @@ export function create (ctx, username, password, opts) {
       const login = Login.offline(ctx.localStorage, username, userId, dataKey)
 
       // Now activate:
-      const request = login.authJson()
+      const auth = login.authJson()
+      const request = {
+        l1: auth.userId,
+        lp1: auth.passwordAuth
+      }
       return ctx.authRequest('POST', '/v1/account/activate', request).then(reply => login)
     })
   })