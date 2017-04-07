e6b4f40fcb8b6f9b61d03b6c92f3de5392614e57
William Swanson <swansontec@gmail.com>
2017-04-07 14:27:07 -0700

    Fix build issues uncovered by `airbitz-cli`

--- src/io/node/index.js
+++ src/io/node/index.js
@@ -1,13 +1,13 @@
 import {IoContext} from '../io.js'
 import crypto from 'crypto'
 import fetch from 'node-fetch'
-import {LocalStorage} from 'node-localstorage'
+import nodeStorage from 'node-localstorage'
 
 export function makeNodeIo (path, opts = {}) {
   const native = {
     console: console,
     fetch: fetch,
-    localStorage: new LocalStorage(path),
+    localStorage: new nodeStorage.LocalStorage(path),
     random: bytes => crypto.randomBytes(bytes)
   }
 
