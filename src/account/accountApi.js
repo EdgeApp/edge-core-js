429970ed1cc68660ae45ba88775bdf3e674243ea
William Swanson <swansontec@gmail.com>
2017-06-07 16:50:57 -0700

    Fix spelling of `Account.username`

--- src/account/accountApi.js
+++ src/account/accountApi.js
@@ -32,7 +32,7 @@ function makeAccountApi (state, loginType) {
     get loginTree () {
       return state.loginTree
     },
-    get usernname () {
+    get username () {
       return state.loginTree.username
     },
 
