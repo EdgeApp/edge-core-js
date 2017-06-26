d4fd058e47e100faa34f88125479757b63fc2f93
William Swanson <swansontec@gmail.com>
2017-06-26 13:42:00 -0700

    Use `rollup-plugin-eslint` on the unit tests
    This is slightly faster than running the linter in its own process.

--- rollup.config.test.js
+++ rollup.config.test.js
@@ -1,9 +1,15 @@
+import eslint from 'rollup-plugin-eslint'
 import config from './rollup.config.js'
 
 export default {
   entry: 'test/all.js',
   external: ['assert', ...config.external],
-  plugins: config.plugins,
+  plugins: [
+    eslint({
+      exclude: 'build/crypto-bundle.js'
+    }),
+    ...config.plugins
+  ],
   targets: [
     {
       dest: 'build/tests.cjs.js',
