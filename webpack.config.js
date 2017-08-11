a82bec2d2648ca15b03b13a6914ce33bd1e2403d
William Swanson <swansontec@gmail.com>
2017-08-11 15:38:51 -0700

    Upgrade build tools & other dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,6 @@ const path = require('path')
 
 module.exports = {
   entry: './src/crypto/bundle.js',
-  module: {
-    loaders: [{ test: /\.json$/, loader: 'json-loader' }]
-  },
   output: {
     filename: './build/crypto-bundle.js',
     libraryTarget: 'commonjs'
