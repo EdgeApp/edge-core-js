7d18124ebb6b480affbfde096189c86c1615d4c0
William Swanson <swansontec@gmail.com>
2017-10-02 20:16:02 -0700

    Avoid redundant build steps

--- rollup.config.test.js
+++ rollup.config.test.js
@@ -1,18 +1,10 @@
-import eslint from 'rollup-plugin-eslint'
 import multiEntry from 'rollup-plugin-multi-entry'
 import config from './rollup.config.js'
 
 export default {
   entry: 'src/**/*.test.js',
   external: config.external,
-  plugins: [
-    eslint({
-      exclude: 'build/crypto-bundle.js',
-      throwOnError: true
-    }),
-    multiEntry(),
-    ...config.plugins
-  ],
+  plugins: [multiEntry(), ...config.plugins],
   targets: [
     {
       dest: 'build/tests.cjs.js',
