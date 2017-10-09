bf7ec4e163dc5d6aaceb92f31e593137cb5d2653
William Swanson <swansontec@gmail.com>
2017-10-09 15:26:23 -0700

    Make the core redux debuggable

--- src/modules/makeStore.js
+++ src/modules/makeStore.js
@@ -1,8 +1,17 @@
-import { applyMiddleware, createStore } from 'redux'
+// @flow
+import { applyMiddleware, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import { reactionMiddleware } from '../util/redux/reaction.js'
 import reducer from './rootReducer.js'
 
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ name: 'core' })
+    : compose
+
 export function makeStore () {
-  return createStore(reducer, applyMiddleware(thunk, reactionMiddleware))
+  return createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(thunk, reactionMiddleware))
+  )
 }
