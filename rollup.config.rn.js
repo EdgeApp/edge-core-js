969e47f5d78bcaf406aac1b4c9f4c974a5cc531a
Paul Puey <paul@airbitz.co>
2017-10-22 18:25:12 -0700

    Do not process source files through babel es2015 for react-native
    On Android we hit a problem whereby es2015 babel plugin will create nested functions inside IF statements which causes an error in RN since "use strict" is on by default. This change creates a separate build for react native that is only processed for flow and spread operators. This fixes Android and tested to still work on iOS.

---
+++ rollup.config.rn.js
@@ -0,0 +1,31 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import packageJson from './package.json'
+
+const babelOpts = {
+  presets: ['flow'],
+  plugins: [
+    'transform-object-rest-spread'
+  ]
+}
+
+export default {
+  entry: 'src/indexABC.js',
+  external: [
+    ...Object.keys(packageJson.dependencies),
+    ...Object.keys(packageJson.devDependencies)
+  ],
+  plugins: [
+    commonjs({
+      include: 'build/crypto-bundle.js'
+    }),
+    babel(babelOpts)
+  ],
+  targets: [
+    {
+      dest: packageJson['react-native'],
+      format: 'es',
+      sourceMap: true
+    }
+  ]
+}
