541a1c978da54f29458f670ce0031c2f17f8dc75
William Swanson <swansontec@gmail.com>
2017-10-23 14:45:20 -0700

    Do not log getter functions

--- src/util/api.js
+++ src/util/api.js
@@ -3,9 +3,9 @@ import { rejectify } from '../util/decorators.js'
 /**
  * Prepares an async API endpoint for consumption by the outside world.
  */
-function asyncApi (f, name) {
+function asyncApi (f, name, log) {
   return function asyncApi (...rest) {
-    console.info(name)
+    if (log) console.info(name)
     const promise = rejectify(f)
       .apply(this, rest)
       .catch(e => {
@@ -29,9 +29,9 @@ function asyncApi (f, name) {
 /**
  * Prepares a sync API endploint for consumption by the outside world.
  */
-function syncApi (f, name) {
+function syncApi (f, name, log) {
   return function syncApi (...rest) {
-    console.info(name)
+    if (log) console.info(name)
     try {
       return f.apply(this, rest)
     } catch (e) {
@@ -48,13 +48,18 @@ function wrapProperty (key, d, className, opts = {}) {
   // Wrap functions:
   if (typeof d.value === 'function') {
     const name = `${className}.${key}`
-    d.value = opts.sync ? syncApi(d.value, name) : asyncApi(d.value, name)
+    const { log = true } = opts
+    d.value = opts.sync
+      ? syncApi(d.value, name, log)
+      : asyncApi(d.value, name, log)
   }
   if (d.get != null) {
-    d.get = syncApi(d.get, `get ${className}.${key}`)
+    const { log = false } = opts
+    d.get = syncApi(d.get, `get ${className}.${key}`, log)
   }
   if (d.set != null) {
-    d.set = syncApi(d.set, `set ${className}.${key}`)
+    const { log = true } = opts
+    d.set = syncApi(d.set, `set ${className}.${key}`, log)
   }
 
   // Properties are read-only by default:
@@ -68,6 +73,14 @@ function wrapProperty (key, d, className, opts = {}) {
 /**
  * Copies the provided object, making its properties ready for use as an API.
  * If a property name starts with `@`, it is treated as an options structure.
+ *
+ * Options include:
+ *   writable - Whether outside code can write to the value
+ *             (defaults to false)
+ *   sync     - Whether the function is synchronous or async
+ *             (defaults to false / asynchronous)
+ *   log      - Whether calls to this function should be logged
+ *              (true for functions, false for getters).
  */
 export function wrapObject (className, object) {
   const out = {}
