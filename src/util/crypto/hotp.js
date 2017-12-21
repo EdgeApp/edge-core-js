44731ebf58558e222f2d42d12f74bb9a374b4ec3
William Swanson <swansontec@gmail.com>
2017-12-21 09:04:26 -0800

    Implement the rfc4226 HTOP algorithm

---
+++ src/util/crypto/hotp.js
@@ -0,0 +1,42 @@
+// @flow
+import { hashjs } from './external.js'
+
+export function numberToBe64 (number: number): Uint8Array {
+  const high = Math.floor(number / 4294967296)
+  return new Uint8Array([
+    (high >> 24) & 0xff,
+    (high >> 16) & 0xff,
+    (high >> 8) & 0xff,
+    high & 0xff,
+    (number >> 24) & 0xff,
+    (number >> 16) & 0xff,
+    (number >> 8) & 0xff,
+    number & 0xff
+  ])
+}
+
+export function hmacSha1 (data: Uint8Array, key: Uint8Array) {
+  const hmac = hashjs.hmac(hashjs.sha1, key)
+  return hmac.update(data).digest()
+}
+
+/**
+ * Implements the rfc4226 HOTP specification.
+ * @param {*} secret The secret value, K, from rfc4226
+ * @param {*} counter The counter, C, from rfc4226
+ * @param {*} digits The number of digits to generate
+ */
+export function hotp (secret: Uint8Array, counter: number, digits: number) {
+  const hmac = hmacSha1(numberToBe64(counter), secret)
+
+  const offset = hmac[19] & 0xf
+  const p =
+    ((hmac[offset] & 0x7f) << 24) |
+    (hmac[offset + 1] << 16) |
+    (hmac[offset + 2] << 8) |
+    hmac[offset + 3]
+  const text = p.toString()
+
+  const padding = Array(digits).join('0')
+  return (padding + text).slice(-digits)
+}
---
+++ src/util/crypto/hotp.test.js
@@ -0,0 +1,68 @@
+// @flow
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+import { base32 } from 'rfc4648'
+
+import { base16, utf8 } from '../encoding.js'
+import { hotp, numberToBe64 } from './hotp.js'
+
+describe('hotp', function () {
+  it('converts numbers to bytes', function () {
+    const cases = [
+      // Powers of 2, plus 1:
+      [1, '0000000000000001'],
+      [257, '0000000000000101'],
+      [65537, '0000000000010001'],
+      [16777217, '0000000001000001'],
+      [4294967297, '0000000100000001'],
+      [1099511627777, '0000010000000001'],
+      [281474976710657, '0001000000000001'],
+      [72057594037927937, '0100000000000000'], // Truncated
+      // The edge of the representable integers:
+      [9007199254740991, '001fffffffffffff'],
+      [9007199254740992, '0020000000000000'],
+      [9007199254740993, '0020000000000000'], // Truncated
+      [9007199254740994, '0020000000000002'],
+      // Fractions:
+      [0.75, '0000000000000000'],
+      [1.75, '0000000000000001'],
+      // Negative numbers:
+      [-1, 'ffffffffffffffff'],
+      [-256, 'ffffffffffffff00'],
+      [-257, 'fffffffffffffeff'],
+      [-4294967296, 'ffffffff00000000'],
+      [-4294967297, 'fffffffeffffffff'],
+      [-9007199254740992, 'ffe0000000000000']
+    ]
+    for (const [number, hex] of cases) {
+      expect(base16.stringify(numberToBe64(number))).to.equal(hex)
+    }
+  })
+
+  it('Handles official rfc4226 test vectors', function () {
+    const key = utf8.parse('12345678901234567890')
+    const cases = [
+      '755224',
+      '287082',
+      '359152',
+      '969429',
+      '338314',
+      '254676',
+      '287922',
+      '162583',
+      '399871',
+      '520489'
+    ]
+
+    for (let i = 0; i < cases.length; ++i) {
+      expect(hotp(key, i, 6)).to.equal(cases[i])
+    }
+  })
+
+  it('Handles leading zeros in output', function () {
+    const key = base32.parse('AAAAAAAA')
+    expect(hotp(key, 2, 6)).to.equal('073348')
+    expect(hotp(key, 9, 6)).to.equal('003773')
+    expect(hotp(key, 41952, 6)).to.equal('048409')
+  })
+})
