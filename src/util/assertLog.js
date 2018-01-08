a1464a8f80ab3f0aa1b922286eaab6e752dc4c86
William Swanson <swansontec@gmail.com>
2018-01-08 15:40:00 -0800

    Improve the assert log utility
    Adds a verbose mode, which will send all log entries to the console.
    
    Also improve types and documentation comments.

--- src/util/assertLog.js
+++ src/util/assertLog.js
@@ -1,25 +1,45 @@
 // @flow
 import { assert as chaiAssert } from 'chai'
 
-function stringify (...args: Array<mixed>) {
+/**
+ * Asserts that a correct sequence of events have occurred.
+ * Used for testing callbacks.
+ *
+ * To log an event, call this function with a string describing the event.
+ * Then, to verify that everything is correct, call the `assert` method
+ * with an array of expected log strings. If there is a mis-match,
+ * `assert` will throw an exception.
+ */
+export type AssertLog = {
+  assert(Array<string>): void
+} & ((...args: Array<any>) => void)
+
+function stringify (...args: Array<any>) {
   return args
     .map(arg => {
       if (arg == null) return typeof arg
-      if (typeof arg !== 'object') return (arg: any).toString()
+      if (typeof arg !== 'object') return arg.toString()
       return JSON.stringify(arg)
     })
     .join(' ')
 }
 
 /**
- * Asserts that the correct events have occurred.
+ * Creates an object that can assert that the correct events have occurred.
  * Used for testing callbacks.
+ * @param sort True to ignore the order of events.
+ * @param verbose True to also send all logged events to the console.
  */
-export function makeAssertLog (sort: boolean = false) {
+export function makeAssertLog (
+  sort: boolean = false,
+  verbose: boolean = false
+): AssertLog {
   let events: Array<string> = []
 
-  const out = function log (...args: Array<mixed>) {
-    events.push(stringify(...args))
+  const out = function log (...args: Array<any>) {
+    const event = stringify(...args)
+    if (verbose) console.log(event)
+    events.push(event)
   }
 
   out.assert = function assert (expected: Array<string>) {
@@ -29,5 +49,5 @@ export function makeAssertLog (sort: boolean = false) {
     events = []
   }
 
-  return out
+  return (out: any)
 }
