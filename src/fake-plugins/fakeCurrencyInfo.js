4933a1b987aa168254908c6a17297dd411069a05
William Swanson <swansontec@gmail.com>
2018-02-05 13:40:18 -0800

    Add flow to the fake currency info

--- src/fake-plugins/fakeCurrencyInfo.js
+++ src/fake-plugins/fakeCurrencyInfo.js
@@ -1,14 +1,29 @@
-export const fakeCurrencyInfo = {
+// @flow
+
+import type { EdgeCurrencyInfo } from '../edge-core-index.js'
+
+export const fakeCurrencyInfo: EdgeCurrencyInfo = {
+  // Basic currency information:
   currencyCode: 'TEST',
+  currencyName: 'test coin',
+  pluginName: 'testcoin',
   denominations: [
     { multiplier: '10', name: 'SMALL' },
     { multiplier: '100', name: 'TEST' }
   ],
+  walletTypes: ['wallet:fakecoin'],
+
+  // Configuration options:
+  defaultSettings: {},
   metaTokens: [
     {
       currencyCode: 'TOKEN',
+      currencyName: 'fake token',
       denominations: [{ multiplier: '1000', name: 'TOKEN' }]
     }
   ],
-  walletTypes: ['wallet:fakecoin']
+
+  // Explorers:
+  addressExplorer: 'https://edgesecure.co',
+  transactionExplorer: 'https://edgesecure.co'
 }
