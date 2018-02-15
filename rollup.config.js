4e07942ca4db511ad2046723be32ecda9f503e3f
William Swanson <swansontec@gmail.com>
2018-02-14 17:39:33 -0800

    Make the core compatible with Webpack

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import alias from 'rollup-plugin-alias'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 
@@ -13,22 +14,48 @@ const babelOpts = {
   ]
 }
 
-export default {
-  external: [
-    'regenerator-runtime/runtime',
-    ...Object.keys(packageJson.dependencies),
-    ...Object.keys(packageJson.devDependencies)
-  ],
-  input: 'src/edge-core-index.js',
-  output: [
-    { file: packageJson.main, format: 'cjs' },
-    { file: packageJson.module, format: 'es' }
-  ],
-  plugins: [
-    commonjs({
-      include: 'build/crypto-bundle.js'
-    }),
-    babel(babelOpts)
-  ],
-  sourcemap: true
+const commonjsOpts = {
+  include: 'build/crypto-bundle.js'
 }
+
+const external = [
+  'regenerator-runtime/runtime',
+  ...Object.keys(packageJson.dependencies),
+  ...Object.keys(packageJson.devDependencies)
+]
+
+export default [
+  {
+    external,
+    input: 'src/edge-core-index.js',
+    output: [
+      { file: packageJson.main, format: 'cjs' },
+      { file: packageJson.module, format: 'es' }
+    ],
+    plugins: [
+      alias({
+        './io/node/node-io.js': 'src/io/node/node-io.js',
+        './io/react-native/react-native-io.js':
+          'src/io/react-native/react-native-dummy.js'
+      }),
+      commonjs(commonjsOpts),
+      babel(babelOpts)
+    ],
+    sourcemap: true
+  },
+  {
+    external,
+    input: 'src/edge-core-index.js',
+    output: { file: packageJson['react-native'], format: 'cjs' },
+    plugins: [
+      alias({
+        './io/node/node-io.js': 'src/io/node/node-dummy.js',
+        './io/react-native/react-native-io.js':
+          'src/io/react-native/react-native-io.js'
+      }),
+      commonjs(commonjsOpts),
+      babel(babelOpts)
+    ],
+    sourcemap: true
+  }
+]
