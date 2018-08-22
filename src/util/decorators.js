0806eaa378559739552ce5ac14f88de8f1cc74ea
William Swanson <swansontec@gmail.com>
2018-08-22 12:32:44 -0700

    Remove the `rejectify` helper

--- src/util/decorators.js
+++ src/util/decorators.js
@@ -1,17 +1,4 @@
 /**
- * If the function f throws an error, return that as a rejected promise.
- */
-export function rejectify (f) {
-  return function rejectify (...rest) {
-    try {
-      return f.apply(this, rest)
-    } catch (e) {
-      return Promise.reject(e)
-    }
-  }
-}
-
-/**
  * Prevents a function from running in parallel.
  * The currently-running operation must finish before the new one starts.
  */
