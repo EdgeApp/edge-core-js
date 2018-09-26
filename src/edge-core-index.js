6e505ee2ccb9b03323ea7b4fcbf1aab2a930436f
Kevin Sullivan <wolverine.ks@gmail.com>
2018-09-26 15:26:10 -0700

    Add test user with wallets

--- src/edge-core-index.js
+++ src/edge-core-index.js
@@ -15,6 +15,7 @@ export { makeFakeIos } from './io/fake/fake-io.js'
 export { makeNodeIo } from './io/node/node-io.js'
 export { makeReactNativeIo } from './io/react-native/react-native-io.js'
 export { fakeUser } from './io/fake/fakeUser.js'
+export { fakeUser1 } from './io/fake/fakeUser1.js'
 export {
   DustSpendError,
   errorNames,
