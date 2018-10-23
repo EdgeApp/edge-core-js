196f7f0c7651c1149d46fbd1389f826e16150b74
William Swanson <swansontec@gmail.com>
2018-10-23 13:51:13 -0700

    Add a `fuzzyTimeout` helper

--- src/util/promise.js
+++ src/util/promise.js
@@ -29,3 +29,59 @@ export function timeout<T> (
   })
   return Promise.race([promise, timeout])
 }
+
+/**
+ * Waits for a collection of promises.
+ * Returns all the promises that manage to resolve within the timeout.
+ * If no promises mange to resolve within the timeout, returns the
+ * first promise that resolves.
+ * If all promises reject, rejects an array of errors.
+ */
+export function fuzzyTimeout<Type> (
+  promises: Array<Promise<Type>>,
+  timeoutMs: number
+): Promise<Array<Type>> {
+  return new Promise((resolve, reject) => {
+    let done = false
+    const results = []
+    const failures = []
+
+    // Set up the timer:
+    let timer = setTimeout(() => {
+      timer = null
+      if (results.length > 0) {
+        done = true
+        resolve(results)
+      }
+    }, timeoutMs)
+
+    function checkEnd () {
+      const allDone = results.length + failures.length === promises.length
+      if (allDone && timer != null) {
+        clearTimeout(timer)
+      }
+      if (allDone || timer == null) {
+        done = true
+        if (results.length > 0) resolve(results)
+        else reject(failures)
+      }
+    }
+    checkEnd() // Handle empty lists
+
+    // Attach to the promises:
+    for (const promise of promises) {
+      promise.then(
+        result => {
+          if (done) return
+          results.push(result)
+          checkEnd()
+        },
+        failure => {
+          if (done) return
+          failures.push(failure)
+          checkEnd()
+        }
+      )
+    }
+  })
+}
---
+++ test/util/promise.test.js
@@ -0,0 +1,28 @@
+// @flow
+
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+
+import { fuzzyTimeout } from '../../src/util/promise.js'
+import { expectRejection } from '../expect-rejection.js'
+
+function snooze (ms: number) {
+  return new Promise(resolve => setTimeout(() => resolve(ms), ms))
+}
+
+describe('promise', function () {
+  it('fuzzyTimeout resolves', async function () {
+    expect(await fuzzyTimeout([snooze(1), snooze(20)], 10)).deep.equals([1])
+    expect(await fuzzyTimeout([snooze(1), snooze(2)], 10)).deep.equals([1, 2])
+    expect(await fuzzyTimeout([snooze(20), snooze(30)], 10)).deep.equals([20])
+
+    const data = [snooze(1), Promise.reject(new Error()), snooze(1000)]
+    expect(await fuzzyTimeout(data, 10)).deep.equals([1])
+
+    const fail = Promise.reject(new Error('Expected'))
+    await expectRejection(
+      fuzzyTimeout([fail, fail], 10),
+      'Error: Expected,Error: Expected'
+    )
+  })
+})
