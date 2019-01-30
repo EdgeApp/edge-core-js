78ba6b690f0f30ed286a3c22e69aada2913fcab8
William Swanson <swansontec@gmail.com>
2019-01-29 16:14:14 -0800

    Tighten up the lobby unit test

--- test/modules/login/lobby.test.js
+++ test/modules/login/lobby.test.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { assert } from 'chai'
+import { expect } from 'chai'
 import elliptic from 'elliptic'
 import { describe, it } from 'mocha'
 
@@ -25,10 +25,9 @@ describe('edge login lobby', function () {
     const pubkey = keypair.getPublic().encodeCompressed()
     const testReply = { testReply: 'This is a test' }
 
-    assert.deepEqual(
-      decryptLobbyReply(keypair, encryptLobbyReply(io, pubkey, testReply)),
-      testReply
-    )
+    expect(
+      decryptLobbyReply(keypair, encryptLobbyReply(io, pubkey, testReply))
+    ).deep.equals(testReply)
   })
 
   it('lobby ping-pong', async function () {
@@ -38,8 +37,8 @@ describe('edge login lobby', function () {
     )
     const i1 = getInternalStuff(context1)
     const i2 = getInternalStuff(context2)
-    const testRequest = { testRequest: 'This is a test' }
-    const testReply = { testReply: 'This is a test' }
+    const testRequest = { testRequest: 'This is a request' }
+    const testReply = { testReply: 'This is a reply' }
 
     return new Promise((resolve, reject) => {
       // Use 10 ms polling to really speed up the test:
@@ -48,15 +47,21 @@ describe('edge login lobby', function () {
           lobby.on('error', reject)
           lobby.watch('replies', (replies: Array<Object>) => {
             if (replies.length === 0) return
-            assert.deepEqual(replies[0], testReply)
             lobby.close()
+            expect(replies[0]).deep.equals(testReply)
             resolve()
           })
 
-          return i2.fetchLobbyRequest(lobby.lobbyId).then(request => {
-            assert.deepEqual(request, testRequest)
-            return i2.sendLobbyReply(lobby.lobbyId, request, testReply)
-          })
+          return i2
+            .fetchLobbyRequest(lobby.lobbyId)
+            .then(request => {
+              expect(request).to.include(testRequest)
+              i2.sendLobbyReply(lobby.lobbyId, request, testReply)
+            })
+            .catch(error => {
+              lobby.close()
+              reject(error)
+            })
         })
         .catch(reject)
     })
