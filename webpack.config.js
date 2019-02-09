2e57ffd645458572a430e37430d2a96e697292f4
William Swanson <swansontec@gmail.com>
2019-02-09 00:19:56 -0800

    Upgrade to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const packageJson = require('./package.json')
 
 const bundledModules = [
@@ -23,6 +25,7 @@ module.exports = {
   devtool: 'source-map',
   entry: './build/react-native.js',
   externals,
+  mode: 'development',
   module: {
     rules: [
       {
@@ -34,6 +37,7 @@ module.exports = {
   },
   output: {
     filename: packageJson['react-native'],
-    libraryTarget: 'commonjs'
+    libraryTarget: 'commonjs',
+    path: path.resolve(__dirname)
   }
 }
