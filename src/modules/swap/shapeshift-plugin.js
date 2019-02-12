ddb80be6a4679121f0576ee29af2b41082026503
William Swanson <swansontec@gmail.com>
2019-02-12 15:10:58 -0800

    Run yarn format

--- src/modules/swap/shapeshift-plugin.js
+++ src/modules/swap/shapeshift-plugin.js
@@ -285,7 +285,8 @@ function makeShapeshiftTools (env: EdgePluginEnvironment): EdgeSwapTools {
       const tx = await fromWallet.makeSpend(spendInfo)
       tx.otherParams.payinAddress = spendInfo.spendTargets[0].publicAddress
       tx.otherParams.uniqueIdentifier = spendInfo.spendTargets[0].otherParams
-        ? spendInfo.spendTargets[0].otherParams.uniqueIdentifier : ''
+        ? spendInfo.spendTargets[0].otherParams.uniqueIdentifier
+        : ''
 
       // Convert that to the output format:
       return makeSwapPluginQuote(
