1a6d69dcf6e606daafb33679302c8e6f309295ec
William Swanson <swansontec@gmail.com>
2019-04-23 15:09:59 -0700

    Remove swap plugins
    These now live in edge-exchange-plugins, so we don't need a second copy.

--- src/core/core.js
+++ src/core/core.js
@@ -1,11 +1,5 @@
 // @flow
 
-import { addEdgeCorePlugins } from './plugins/plugins-actions.js'
-import { makeChangellyPlugin } from './swap/changelly-plugin.js'
-import { makeChangeNowPlugin } from './swap/changenow-plugin.js'
-import { makeFaastPlugin } from './swap/faast-plugin.js'
-import { makeShapeshiftPlugin } from './swap/shapeshift-plugin.js'
-
 export { makeFakeIo } from './fake/fake-io.js'
 export { makeFakeWorld } from './fake/fake-world.js'
 export {
@@ -13,10 +7,3 @@ export {
   lockEdgeCorePlugins
 } from './plugins/plugins-actions.js'
 export { closeEdge, makeContext } from './root.js'
-
-addEdgeCorePlugins({
-  changelly: makeChangellyPlugin,
-  changenow: makeChangeNowPlugin,
-  faast: makeFaastPlugin,
-  shapeshift: makeShapeshiftPlugin
-})
