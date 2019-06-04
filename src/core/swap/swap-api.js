60355a9be1d801e64d00e6221c70325039f16a7f
William Swanson <swansontec@gmail.com>
2019-06-04 12:34:15 -0700

    Re-prioritize the swap error logic

--- src/core/swap/swap-api.js
+++ src/core/swap/swap-api.js
@@ -98,10 +98,10 @@ export async function fetchSwapQuote (
  * Ranks different error codes by priority.
  */
 function rankError (error: Object) {
-  if (error.name === errorNames.SwapBelowLimitError) return 5
-  if (error.name === errorNames.SwapAboveLimitError) return 4
-  if (error.name === errorNames.InsufficientFundsError) return 3
-  if (error.name === errorNames.PendingFundsError) return 3
+  if (error.name === errorNames.InsufficientFundsError) return 5
+  if (error.name === errorNames.PendingFundsError) return 5
+  if (error.name === errorNames.SwapBelowLimitError) return 4
+  if (error.name === errorNames.SwapAboveLimitError) return 3
   if (error.name === errorNames.SwapPermissionError) return 2
   if (error.name === errorNames.SwapCurrencyError) return 1
   return 0
