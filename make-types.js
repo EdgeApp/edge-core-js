bf02a02e76c1190bb2b4bad24f83219b936bac30
William Swanson <swansontec@gmail.com>
2020-02-26 06:03:48 -0800

    Fix the Typescript definitions

--- make-types.js
+++ make-types.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const babel = require('@babel/core')
-const fs = require('fs')
+const { makeNodeDisklet } = require('disklet')
 const eslint = require('eslint')
 
 function jsToTs(code) {
@@ -21,29 +21,45 @@ function jsToTs(code) {
   return output
 }
 
-// Transpile errors to plain Javascript and TypeScript:
-const errorFile = fs.readFileSync('src/types/error.js', 'utf8')
-const errorJs = babel.transformSync(errorFile, {
-  presets: ['@babel/preset-flow'],
-  plugins: [
-    '@babel/plugin-transform-modules-commonjs',
-    'babel-plugin-transform-fake-error-class'
-  ]
-}).code
-fs.writeFileSync('types.js', errorJs)
-fs.writeFileSync('src/types/error.ts', jsToTs(errorFile))
-
-// Transpile Flow types to Typescript:
-const typesFile = fs.readFileSync('src/types/types.js', 'utf8')
-fs.writeFileSync('src/types/types.ts', jsToTs(typesFile))
-
-// Fix the files with ESLint:
-const cli = new eslint.CLIEngine({ fix: true, ignore: false })
-const report = cli.executeOnFiles(['./src/types/*.ts'])
-eslint.CLIEngine.outputFixes(report)
-if (eslint.CLIEngine.getErrorResults(report.results).length > 0) {
-  console.error(
-    'Error: Conversion to TypeScript failed. Please run `yarn lint` to see errors.'
+async function main() {
+  const disklet = makeNodeDisklet('.')
+  await disklet.setText(
+    'lib/types/index.ts',
+    "export * from './types'\n" + "export * from './exports'\n"
   )
-  process.exit(1)
+
+  // Transpile errors to plain Javascript and TypeScript:
+  const errorFile = await disklet.getText('src/types/error.js', 'utf8')
+  const errorJs = babel.transformSync(errorFile, {
+    presets: ['@babel/preset-flow'],
+    plugins: [
+      '@babel/plugin-transform-modules-commonjs',
+      'babel-plugin-transform-fake-error-class'
+    ]
+  }).code
+  await disklet.setText('types.js', errorJs)
+  await disklet.setText('lib/types/error.ts', jsToTs(errorFile))
+
+  // Transpile entry functions to Typescript:
+  const exportsFile = await disklet.getText('src/types/exports.js', 'utf8')
+  await disklet.setText('lib/types/exports.ts', jsToTs(exportsFile))
+
+  // Transpile Flow types to Typescript:
+  const typesFile = await disklet.getText('src/types/types.js', 'utf8')
+  await disklet.setText('lib/types/types.ts', jsToTs(typesFile))
+
+  // Fix the files with ESLint:
+  const cli = new eslint.CLIEngine({ fix: true, ignore: false })
+  const report = cli.executeOnFiles(['./lib/types/*.ts'])
+  eslint.CLIEngine.outputFixes(report)
+  if (eslint.CLIEngine.getErrorResults(report.results).length > 0) {
+    throw new Error(
+      'Conversion to TypeScript failed. Please run `npx eslint --no-ignore lib/types/*.ts` to see errors.'
+    )
+  }
 }
+
+main().catch(e => {
+  console.error(e)
+  process.exit(1)
+})
---
+++ src/types/exports.js
@@ -0,0 +1,60 @@
+import {
+  type EdgeContext,
+  type EdgeContextOptions,
+  type EdgeCorePlugins,
+  type EdgeFakeUser,
+  type EdgeFakeWorld,
+  type EdgeIo,
+  type EdgeLoginMessages,
+  type EdgeNativeIo
+} from './types.js'
+
+const hack: any = null
+
+export const addEdgeCorePlugins = (plugins: EdgeCorePlugins): mixed => hack
+export const lockEdgeCorePlugins = (): mixed => hack
+export const closeEdge = (): mixed => hack
+export const makeFakeIo = (): EdgeIo => hack
+
+/**
+ * Initializes the Edge core library,
+ * automatically selecting the appropriate platform.
+ */
+export const makeEdgeContext = (
+  opts: EdgeContextOptions
+): Promise<EdgeContext> => hack
+
+export const makeFakeEdgeWorld = (
+  users?: EdgeFakeUser[]
+): Promise<EdgeFakeWorld> => hack
+
+/**
+ * React Native component for creating an EdgeContext.
+ */
+export const MakeEdgeContext = (props: {
+  debug?: boolean,
+  nativeIo?: EdgeNativeIo,
+  onError?: (e: any) => mixed,
+  onLoad: (context: EdgeContext) => mixed,
+  options: EdgeContextOptions
+}): any => hack // React element
+
+/**
+ * React Native component for creating an EdgeFakeWorld for testing.
+ */
+export const MakeFakeEdgeWorld = (props: {
+  debug?: boolean,
+  nativeIo?: EdgeNativeIo,
+  onError?: (e: any) => mixed,
+  onLoad: (context: EdgeContext) => mixed,
+  users: EdgeFakeUser[]
+}): any => hack // React element
+
+/**
+ * React Native function for getting login alerts without a context:
+ */
+export const fetchLoginMessages = (apiKey: string): EdgeLoginMessages => hack
+
+// System-specific io exports:
+export const makeBrowserIo = (): EdgeIo => hack
+export const makeNodeIo = (path: string): EdgeIo => hack
