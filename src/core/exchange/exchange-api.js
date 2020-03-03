cfeedd3dc64f45c0877d3b5dc14845516cfa4c4c
William Swanson <swansontec@gmail.com>
2020-03-03 14:31:25 -0800

    Clean up bias code

--- src/core/exchange/exchange-api.js
+++ src/core/exchange/exchange-api.js
@@ -19,23 +19,6 @@ const biasDefaults = {
  * Creates an unwrapped exchange cache API object.
  */
 export function makeExchangeCache(ai: ApiInput): EdgeRateCache {
-  /**
-   * TODO: Once the user has an exchange-rate preference,
-   * look that up and bias in favor of the preferred exchange.
-   */
-  function makeGetPairCost(biases?: { [name: string]: number }) {
-    return (source, age, inverse) => {
-      // The age curve goes from 0 to 1, with 1 being infinitely old.
-      // The curve reaches half way (0.5) at 30 seconds in:
-      const ageCurve = age / (30 + age)
-      let bias = biases ? biases[source] : biasDefaults[source]
-      if (typeof bias !== 'number') {
-        bias = 0
-      }
-      return ageCurve + bias + (inverse ? 1.1 : 1) // + 2 * isWrongExchange()
-    }
-  }
-
   const out: EdgeRateCache = {
     on: onMethod,
 
@@ -45,11 +28,25 @@ export function makeExchangeCache(ai: ApiInput): EdgeRateCache {
       amount: number = 1,
       opts: EdgeConvertCurrencyOpts = {}
     ): Promise<number> {
+      const { biases = biasDefaults } = opts
+
+      function getPairCost(
+        source: string,
+        age: number,
+        inverse: boolean
+      ): number {
+        // The age curve goes from 0 to 1, with 1 being infinitely old.
+        // The curve reaches half way (0.5) at 30 seconds in:
+        const ageCurve = age / (30 + age)
+        const bias = biases[source] != null ? biases[source] : 0
+        return ageCurve + bias + (inverse ? 1.1 : 1)
+      }
+
       const rate = getExchangeRate(
         ai.props.state,
         fromCurrency,
         toCurrency,
-        makeGetPairCost(opts.biases)
+        getPairCost
       )
       return amount * rate
     }
