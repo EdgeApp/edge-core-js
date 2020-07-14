951d911ad98abf85926e4c0ac3b2556d1685e3ed
William Swanson <swansontec@gmail.com>
2020-07-14 15:42:45 -0700

    Allow users to pass 6-digit OTP codes directly
    Before, we only allowed the user to type in their 16-character backup code, which is less secure with screen readers.
    
    Also, we called this option `otp`, instead of `otpKey` like it should have been. This commit still allows the old practice of passing `otpKey` as `otp` for compatiblity, but that way is deprecated now.
    
    The login UI still requires changes to actually expose this feature.

--- src/types/types.js
+++ src/types/types.js
@@ -769,7 +769,8 @@ export type EdgeRatePlugin = {
 // ---------------------------------------------------------------------
 
 export type EdgeAccountOptions = {
-  otp?: string
+  otpKey?: string, // The OTP secret
+  otp?: string // The 6-digit OTP, or (deprecated) the OTP secret
 }
 
 // currencies ----------------------------------------------------------
