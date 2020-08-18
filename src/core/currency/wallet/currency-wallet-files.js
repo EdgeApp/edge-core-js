749f4a426a3c127b85c2cf6698ecfb6d6854ef58
William Swanson <swansontec@gmail.com>
2020-08-18 14:58:12 -0700

    Remove some unnecessary casts to `any`

--- src/core/currency/wallet/currency-wallet-files.js
+++ src/core/currency/wallet/currency-wallet-files.js
@@ -234,9 +234,8 @@ function loadNameFile(
     .getText()
     .then(text => JSON.parse(text).walletName)
     .catch(async e => {
-      // The wallet info does happen to have full data, so this works:
-      const fullWalletInfo: any = input.props.selfState.walletInfo
-      const name = await fetchBackupName(input, fullWalletInfo.appIds || [])
+      const { appIds = [] } = input.props.selfState.walletInfo
+      const name = await fetchBackupName(input, appIds)
       if (name != null) await renameCurrencyWallet(input, name)
       return name
     })
