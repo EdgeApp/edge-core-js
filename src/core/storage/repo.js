d27bf4dea81a3e81b8a3d34200fc9ca93d23b1c2
Samuel Holmes <sam@edge.app>
2021-09-09 16:47:42 -0700

    Limit the maximum of files in changes per syncRepo request
    This should alleviate max payload errors encountered by the server.

--- src/core/storage/repo.js
+++ src/core/storage/repo.js
@@ -1,12 +1,6 @@
 // @flow
 
-import {
-  type Disklet,
-  downgradeDisklet,
-  mapAllFiles,
-  mergeDisklets,
-  navigateDisklet
-} from 'disklet'
+import { type Disklet, mergeDisklets, navigateDisklet } from 'disklet'
 import { type SyncClient } from 'edge-sync-client'
 import { base16 } from 'rfc4648'
 
@@ -18,6 +12,8 @@ import {
   type StorageWalletPaths,
   type StorageWalletStatus
 } from './storage-reducer.js'
+
+const CHANGESET_MAX_ENTRIES = 100
 
 export type SyncResult = {
   changes: { [path: string]: any },
@@ -88,49 +84,83 @@ export async function saveChanges(
 /**
  * Synchronizes the local store with the remote server.
  */
-export function syncRepo(
+export async function syncRepo(
   syncClient: SyncClient,
   paths: StorageWalletPaths,
   status: StorageWalletStatus
 ): Promise<SyncResult> {
   const { changesDisklet, dataDisklet, syncKey } = paths
-  const changesFolder = downgradeDisklet(changesDisklet)
 
-  return mapAllFiles(changesFolder, (file, name) =>
-    file.getText().then(text => ({ file, name, json: JSON.parse(text) }))
-  ).then(async ourChanges => {
-    const syncKeyEncoded = base16.stringify(syncKey).toLowerCase()
+  const ourChanges: Array<{
+    path: string,
+    json: any
+  }> = await deepListWithLimit(changesDisklet).then(paths => {
+    return Promise.all(
+      paths.map(async path => ({
+        path,
+        json: JSON.parse(await changesDisklet.getText(path))
+      }))
+    )
+  })
 
-    // Send a read request if no changes present locally, otherwise bundle the
-    // changes with the a update request.
-    const reply = await (() => {
-      // Read the repo if no changes present locally.
-      if (ourChanges.length === 0) {
-        return syncClient.readRepo(syncKeyEncoded, status.lastHash)
-      }
+  const syncKeyEncoded = base16.stringify(syncKey).toLowerCase()
 
-      // Write local changes to the repo.
-      const changes: { [name: string]: any } = {}
-      for (const change of ourChanges) {
-        changes[change.name] = change.json
-      }
-      return syncClient.updateRepo(syncKeyEncoded, status.lastHash, { changes })
-    })()
+  // Send a read request if no changes present locally, otherwise bundle the
+  // changes with the a update request.
+  const reply = await (() => {
+    // Read the repo if no changes present locally.
+    if (ourChanges.length === 0) {
+      return syncClient.readRepo(syncKeyEncoded, status.lastHash)
+    }
 
-    // Make the request:
-    const { changes = {}, hash } = reply
+    // Write local changes to the repo.
+    const changes: { [name: string]: any } = {}
+    for (const change of ourChanges) {
+      changes[change.path] = change.json
+    }
+    return syncClient.updateRepo(syncKeyEncoded, status.lastHash, { changes })
+  })()
 
-    // Save the incoming changes into our `data` folder:
-    await saveChanges(dataDisklet, changes)
+  // Make the request:
+  const { changes = {}, hash } = reply
 
-    // Delete any changed keys (since the upload is done):
-    await Promise.all(ourChanges.map(change => change.file.delete()))
+  // Save the incoming changes into our `data` folder:
+  await saveChanges(dataDisklet, changes)
 
-    // Update the repo status:
-    status.lastSync = Date.now() / 1000
-    if (hash != null) status.lastHash = hash
-    return paths.baseDisklet
-      .setText('status.json', JSON.stringify(status))
-      .then(() => ({ status, changes }))
+  // Delete any changed keys (since the upload is done):
+  await Promise.all(
+    ourChanges.map(change => changesDisklet.delete(change.path))
+  )
+
+  // Update the repo status:
+  status.lastSync = Date.now() / 1000
+  if (hash != null) status.lastHash = hash
+  return await paths.baseDisklet
+    .setText('status.json', JSON.stringify(status))
+    .then(() => ({ status, changes }))
+}
+
+/**
+ * Lists all files in a disklet, recursively up to a limit.
+ * Returns a list of full paths.
+ */
+async function deepListWithLimit(
+  disklet: Disklet,
+  path: string = '',
+  limit: number = CHANGESET_MAX_ENTRIES
+): Promise<string[]> {
+  return await disklet.list(path).then(async list => {
+    const paths = Object.keys(list).filter(path => list[path] === 'file')
+    const folders = Object.keys(list).filter(path => list[path] === 'folder')
+
+    // Loop over folders to get subpaths
+    for (const folder of folders) {
+      if (paths.length >= limit) break
+      const remaining = limit - paths.length
+      const subpaths = await deepListWithLimit(disklet, folder, remaining)
+      paths.push(...subpaths.slice(0, remaining))
+    }
+
+    return paths
   })
 }
