f05188cde260a690bb33536f3a312a7492c1588c
William Swanson <swansontec@gmail.com>
2024-07-02 15:40:54 -0700

    Move native methods to their own object

---
+++ android/src/main/java/app/edge/reactnative/core/EdgeNative.java
@@ -0,0 +1,92 @@
+package app.edge.reactnative.core;
+
+import android.util.Base64;
+import java.io.File;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+class EdgeNative {
+  private final Disklet mDisklet;
+  private final ExecutorService mPool = Executors.newCachedThreadPool();
+
+  public EdgeNative(File base) {
+    mDisklet = new Disklet(base);
+  }
+
+  public void call(final String name, final String args, PendingCall promise) {
+    mPool.execute(
+        new Runnable() {
+          @Override
+          public void run() {
+            try {
+              handleCall(name, new JSONArray(args), promise);
+            } catch (Throwable error) {
+              promise.reject(error.getMessage());
+            }
+          }
+        });
+  }
+
+  private void handleCall(final String name, JSONArray args, PendingCall promise)
+      throws IOException, JSONException {
+    switch (name) {
+      case "diskletDelete":
+        mDisklet.delete(args.getString(0));
+        promise.resolve(null);
+        break;
+      case "diskletGetData":
+        promise.resolve(Base64.encodeToString(mDisklet.getData(args.getString(0)), Base64.NO_WRAP));
+        break;
+      case "diskletGetText":
+        promise.resolve(mDisklet.getText(args.getString(0)));
+        break;
+      case "diskletList":
+        promise.resolve(new JSONObject(mDisklet.list(args.getString(0))));
+        break;
+      case "diskletSetData":
+        mDisklet.setData(args.getString(0), Base64.decode(args.getString(1), Base64.DEFAULT));
+        promise.resolve(null);
+        break;
+      case "diskletSetText":
+        mDisklet.setText(args.getString(0), args.getString(1));
+        promise.resolve(null);
+        break;
+      case "fetch":
+        break;
+      case "randomBytes":
+        {
+          SecureRandom sr = new SecureRandom();
+          byte[] entropy = new byte[args.getInt(0)];
+          sr.nextBytes(entropy);
+          promise.resolve(Base64.encodeToString(entropy, Base64.NO_WRAP));
+        }
+        break;
+      case "scrypt":
+        {
+          byte[] data = Base64.decode(args.getString(0), Base64.DEFAULT);
+          byte[] salt = Base64.decode(args.getString(1), Base64.DEFAULT);
+          int n = args.getInt(2);
+          int r = args.getInt(3);
+          int p = args.getInt(4);
+          int dklen = args.getInt(5);
+          byte[] out = scrypt(data, salt, n, r, p, dklen);
+          if (out == null) promise.reject("Failed scrypt");
+          else promise.resolve(Base64.encodeToString(out, Base64.NO_WRAP));
+        }
+        break;
+      default:
+        promise.reject("No method " + name);
+    }
+  }
+
+  private native byte[] scrypt(byte[] data, byte[] salt, int n, int r, int p, int dklen);
+
+  static {
+    System.loadLibrary("edge-core-jni");
+  }
+}
---
+++ ios/EdgeNative.swift
@@ -0,0 +1,83 @@
+class EdgeNative {
+  let queue = DispatchQueue(label: "app.edge.reactnative.core")
+  let disklet = Disklet()
+
+  public func call(
+    _ name: String,
+    args: NSArray,
+    promise: PendingCall
+  ) {
+    return queue.async {
+      do {
+        try self.handleCall(name, args: args, promise: promise)
+      } catch {
+        promise.reject("\(error)")
+      }
+    }
+  }
+
+  func handleCall(
+    _ name: String,
+    args: NSArray,
+    promise: PendingCall
+  ) throws {
+    if name == "diskletDelete", let path = args[0] as? String {
+      try disklet.delete(path: path)
+      return promise.resolve(nil)
+    }
+    if name == "diskletGetData", let path = args[0] as? String {
+      return promise.resolve(try disklet.getData(path: path).base64EncodedString())
+    }
+    if name == "diskletGetText", let path = args[0] as? String {
+      return promise.resolve(try disklet.getText(path: path))
+    }
+    if name == "diskletList", let path = args[0] as? String {
+      return promise.resolve(try disklet.list(path: path) as NSDictionary)
+    }
+    if name == "diskletSetData",
+      let path = args[0] as? String,
+      let base64 = args[1] as? String,
+      let data = Data.init(base64Encoded: base64)
+    {
+      try disklet.setData(path: path, data: data)
+      return promise.resolve(nil)
+    }
+    if name == "diskletSetText",
+      let path = args[0] as? String,
+      let text = args[1] as? String
+    {
+      try disklet.setText(path: path, text: text)
+      return promise.resolve(nil)
+    }
+    if name == "randomBytes", let size = args[0] as? Int {
+      if let entropy = NSMutableData(length: size),
+        SecRandomCopyBytes(kSecRandomDefault, size, entropy.mutableBytes) == errSecSuccess
+      {
+        return promise.resolve(entropy.base64EncodedString())
+      }
+      return promise.reject("Could not obtain secure entropy")
+    }
+    if name == "scrypt",
+      let data64 = args[0] as? String,
+      let salt64 = args[1] as? String,
+      let n = args[2] as? UInt64,
+      let r = args[3] as? UInt32,
+      let p = args[4] as? UInt32,
+      let dklen = args[5] as? Int,
+      let data = NSData.init(base64Encoded: data64),
+      let salt = NSData.init(base64Encoded: salt64),
+      let out = NSMutableData(length: dklen)
+    {
+      if crypto_scrypt(
+        data.bytes.bindMemory(to: UInt8.self, capacity: data.length), data.length,
+        salt.bytes.bindMemory(to: UInt8.self, capacity: salt.length), salt.length,
+        n, r, p,
+        out.mutableBytes.bindMemory(to: UInt8.self, capacity: dklen), dklen
+      ) != 0 {
+        return promise.reject("Failed scrypt")
+      }
+      return promise.resolve(out.base64EncodedString())
+    }
+    return promise.reject("No method \(name)")
+  }
+}
