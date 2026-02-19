46dc32c9c06f55a2f2f5656c11feb63140f44585
Matthew <matthew@edge.app>
2026-02-19 15:09:16 -0800

    Make currencyCode access safer

--- src/core/currency/wallet/currency-wallet-reducer.ts
+++ src/core/currency/wallet/currency-wallet-reducer.ts
@@ -350,8 +350,16 @@ const currencyWalletInner = buildReducer<
       const out: EdgeBalances = {}
       for (const tokenId of balanceMap.keys()) {
         const balance = balanceMap.get(tokenId)
-        const { currencyCode } =
-          tokenId == null ? currencyInfo : allTokens[tokenId]
+        let currencyCode: string
+        if (tokenId == null) {
+          currencyCode = currencyInfo.currencyCode
+        } else {
+          if (allTokens[tokenId] == null) {
+            continue
+          }
+          currencyCode = allTokens[tokenId].currencyCode
+        }
+
         if (balance != null) out[currencyCode] = balance
       }
       return out
